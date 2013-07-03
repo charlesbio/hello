b805e6131aaed914ba38ee288d1ca4f0ee5ce58b
charlesbio <charles.bio@havocdigital.com>
2013-07-04 00:02:52 +0800

    gg

--- node.js
+++ node.js
@@ -1,6 +1,27 @@
 #!/usr/bin/env node
 var fs = require('fs');
-var outfile = "hello.txt";
-var out = "A startup is a business built to grow rapidly.\n";
-fs.writeFileSync(outfile, out);  
+var outfile = "prime.txt";
+
+
+var loop = 100;
+
+var fibonacci = function(n) {
+if(n < 1) {return 0; }
+else if(n == 1 || n == 2) { return 1; }
+else if(n >2) { return fibonacci(n - 1) + fibonacci(n - 2); }
+};
+
+
+var firstkfib = function(k) {
+var i = 1;
+var arr = [];
+for(i = 1; i < k+1; i++){
+arr.push(fibonacci(i));	
+}
+return arr;
+	
+};
+
+var output =firstkfib(b);
+fs.writeFileSync(outfile, output);  
 console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
